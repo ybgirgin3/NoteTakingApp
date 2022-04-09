887a4b7785dab2e5e231b4f6a08d985aac320df3
ybgirgin3 <ybgirgin3@gmail.com>
2022-04-10 01:54:55 +0300

    rendering her component için özel hale getirildi

--- frontend/src/Components/Content.js
+++ frontend/src/Components/Content.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import DataRenderer from './DataRendering/DataRenderer';
+import useFetch from './useFetch'
 
 function Content() {
-  const url = `api/v1/list`;
-  const method = 'get';
-  const title = 'Content';
+  
+  const title = 'Content'
+
+  const options = {
+    url : `api/v1/list`,
+    method : 'get',
+  }
+
+  const data = useFetch(options)
+
+  // content kısmını burda renderla
   return (
     <div>
         <div className="title">{title.toUpperCase()}</div>
-      <DataRenderer title={title} url={url} method={method} />
+    {data.map((item, index) => (
+      <li>{item.id} - {item.name}</li>
+    ))}
     </div>
   );
 }
